c6165f55e80526287c5ea2e7e89d939582cddf24
Benjamin Karsono <bkarsono@umich.edu>
2025-04-08 13:20:07 -0400

    Add projects tab

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ createApp({
   data() {
     return {
       currentTab: "About Me",
+      projects: [
+        {
+          name: "Haki 2D",
+          imgUrl: "./assets/logo-haki.png",
+          imgHref: "https://fortnite.gg/creative?creator=nachoboy999",
+          description:
+            "A Brawl Stars clone made in Unreal Editor for Fortnite.",
+        },
+        {
+          name: "Not Stake",
+          imgUrl: "./assets/logo-discord.jpg",
+          imgHref: "https://discord.com/",
+          description:
+            "A Discord bot that allows users to create events and predict outcomes using virtual currency.",
+        },
+        {
+          name: "Draft Stars",
+          imgUrl: "./assets/logo-brawl.jpg",
+          imgHref: "https://supercell.com/en/games/brawlstars/",
+          description: "In Progress...",
+        },
+      ],
     };
   },
   methods: {},
